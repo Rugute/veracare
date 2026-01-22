61497322d497709aef51b0f7c6dd0dee4b07740a
Rugute <rrugute@gmail.com>
2026-01-22 19:53:27 +0300

    added search by code

--- app/api/company/[id]/route.ts
+++ app/api/company/[id]/route.ts
@@ -75,3 +75,24 @@ export async function PATCH(req: Request, ctx: { params: Promise<{ id: string }>
     return new Response("Internal Server Error", { status: 500 });
   }
 }
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { code: string } }
+) {
+  try {
+    const cid = params.code;
+    const company = await prisma.company.findFirst({
+      where: { code: cid },
+    });
+
+    if (!company) {
+      return new Response("Company not found", { status: 404 });
+    }
+
+    return Response.json(company);
+  } catch (err) {
+    console.error("GET error:", err);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
