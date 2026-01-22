e58778588dd1ad140adcda383571a4b3841571ae
Rugute <rrugute@gmail.com>
2026-01-22 18:53:44 +0300

    Added users more fields

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,5 @@
 
-export const dynamic = "force-dynamic"; // ⬅️ Add this line
+export const dynamic = "force-dynamic"; 
 
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
--- prisma/migrations/20260122155025_init/migration.sql
+++ prisma/migrations/20260122155025_init/migration.sql
@@ -1,26 +1,36 @@
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `companyId` INTEGER NULL,
-    ADD COLUMN `firstName` VARCHAR(191) NULL,
-    ADD COLUMN `gender` VARCHAR(191) NULL,
-    ADD COLUMN `lastName` VARCHAR(191) NULL,
-    ADD COLUMN `phone` VARCHAR(191) NULL,
-    MODIFY `password` VARCHAR(191) NULL;
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `firstName` VARCHAR(191) NULL,
+    `lastName` VARCHAR(191) NULL,
+    `gender` VARCHAR(191) NULL,
+    `phone` VARCHAR(191) NULL,
+    `companyId` INTEGER NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Company` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `uuid` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(191) NULL,
     `name` VARCHAR(191) NULL,
     `status` VARCHAR(191) NULL,
-    `license` VARCHAR(191) NULL,
     `logo` VARCHAR(191) NULL,
     `location` VARCHAR(191) NULL,
     `address` VARCHAR(191) NULL,
     `email` VARCHAR(191) NOT NULL,
     `phone` VARCHAR(191) NULL,
+    `createdBy` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `voided` INTEGER NOT NULL DEFAULT 0,
 
     UNIQUE INDEX `Company_uuid_key`(`uuid`),
-    UNIQUE INDEX `Company_license_key`(`license`),
     UNIQUE INDEX `Company_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
