3e1ed58fe492488535b2e2f35f4c113224a22795
gklelei <leleigideon97@gmail.com>
2026-01-22 19:33:07 +0300

    fixed bugs on signin api client

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
+
 import { ThemeProvider } from "@/Modules/Themes/ThemeProvider";
 import { ReactQueryProvider } from "@/lib/ReactQuery";
 
